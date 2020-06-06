8363b18d8eceeb9079d89d966efb69d453bcfdf7
Mk-07 <manishkumartlw07@gmail.com>
2020-06-06 17:06:04 +0530

    first

--- blog_website/blog_website/urls.py
+++ blog_website/blog_website/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""blog_website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from blog import views
+
+app_name = 'blog'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls',)),
+    path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(template_name='blog/post_list.html'), name='logout', kwargs={'next_page':'/'}),
+    # path('register/', views.register, name='register')
+
 ]
